9b01ca769e35144e2a49ea4dd73cf96c18957e21
Nikolas <60915937+NikolasK88@users.noreply.github.com>
2021-03-14 17:52:28 +0200

    Update getAlias.py

--- alias/inc/getAlias.py
+++ alias/inc/getAlias.py
@@ -7,14 +7,18 @@ def get_alias(request):
     date_from = request.GET.get('date_from', False)
     date_to = request.GET.get('date_to', False)
 
+    # if you won't put any filters, you'll get all objects that we have
     alias_obj = Alias.objects.all()
 
     if target:
+        # filter by target
         alias_obj = alias_obj.filter(target=target)
     if date_from:
+        # filter by date_from
         date_from = datetime.strptime(date_from, '%Y-%m-%d %H:%M:%S.%f')
         alias_obj = alias_obj.filter(start__gte=date_from)
     if date_to:
+        # filter by date_to
         date_to = datetime.strptime(date_to, '%Y-%m-%d %H:%M:%S.%f')
         alias_obj = alias_obj.filter(end__lt=date_to)
 
