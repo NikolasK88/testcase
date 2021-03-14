1bb8bc0fa65d869a58ef8b93e4f3f243ba9b6a9f
Nikolas <60915937+NikolasK88@users.noreply.github.com>
2021-03-14 17:51:59 +0200

    Update checkAlias.py

--- alias/inc/checkAlias.py
+++ alias/inc/checkAlias.py
@@ -3,21 +3,27 @@ from alias.models import Alias
 
 
 def check_alias(alias, target, date_from, date_to):
-    
+
+    # checking are there any object which has the same target and  alias as a creating obj
     try:
         alias_obj = Alias.objects.filter(target=target, alias=alias)
         return {'success': 0,
                 'message': 'You already have such object'}
     except:
         pass
-
+    
+    # checking are there any object which run in the same time as a creating obj
     try:
-        alias_obj = Alias.objects.filter(target=target, alias=alias, date_from__lte=date_to)
+        alias_obj = Alias.objects.filter(target=target,
+                                         alias=alias,
+                                         date_from__lte=date_to)
         if date_to:
             try:
-                if Alias.objects.filter(target=target, alias=alias, date_to__gte=date_from):
-                    return {'success': 0,
-                            'message': 'Time was already taken, choose another time'}
+                alias_obj = Alias.objects.filter(target=target,
+                                                 alias=alias,
+                                                 date_to__gte=date_from)
+                return {'success': 0,
+                        'message': 'Time was already taken, choose another time'}
             except:
                 pass
 
