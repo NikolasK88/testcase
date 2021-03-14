3830be774f8963d60ed79b34c67be7a623ecbb8e
Nikolas <60915937+NikolasK88@users.noreply.github.com>
2021-03-14 17:52:49 +0200

    Update replaceAlias.py

--- alias/inc/replaceAlias.py
+++ alias/inc/replaceAlias.py
@@ -5,8 +5,10 @@ from datetime import datetime, timedelta
 def alias_replace(existing_alias, replace_at, new_alias_value):
     replace_at = datetime.strptime(replace_at, '%Y-%m-%dT%H:%M:%S')
     alias_obj = Alias.objects.get(id=int(existing_alias))
+    # changing time for existing_alias obj
     alias_obj.end = replace_at - timedelta(microseconds=1)
     alias_obj.save()
+    # creating a new obj
     new_alias_obj = Alias.objects.create(alias=new_alias_value,
                                          target=alias_obj.target,
                                          start=replace_at,
